69eab0b539fbc0582acce162fc110baa48b81d75
Michael Mathew <mmathew209@gmail.com>
2015-11-21 00:34:43 -0800

    Added Infrastructure for 2 more graphs

--- scripts/DoodleGraphing.js
+++ scripts/DoodleGraphing.js
@@ -1,9 +1,13 @@
 var ScorePerLifeChartDPS = [];
+var Chart3DPS            = [];
+var Chart2DPS            = [];
 var ScorePerLifeChart;
+var Chart2;
+var Chart3;
 
 window.onload = function () {
 
-  ScorePerLifeChart = new CanvasJS.Chart("chartContainer",
+  ScorePerLifeChart = new CanvasJS.Chart("chartScorePerLifeContainer",
   {
     title:{
       text: "Score Per Life"
@@ -20,9 +24,47 @@ window.onload = function () {
     }]
   });
 
+  Chart2 = new CanvasJS.Chart("chart2",
+  {
+    title:{
+      text: "Chart 2"
+    },
+    axisX: {
+      title: "X Label"
+    },
+    axisY: {
+      title: "Y Label"
+    },
+    data: [{
+      type: "line",
+      dataPoints: Chart2DPS
+    }]
+  });
+
+  Chart3 = new CanvasJS.Chart("chart3",
+  {
+    title:{
+      text: "Chart 3"
+    },
+    axisX: {
+      title: "X Label"
+    },
+    axisY: {
+      title: "Y Label"
+    },
+    data: [{
+      type: "line",
+      dataPoints: Chart3DPS
+    }]
+  });
+
   ScorePerLifeChart.render();
+  Chart2.render();
+  Chart3.render();
 }
 
 var updateChart = function() {
   ScorePerLifeChart.render();
+  Chart2.render();
+  Chart3.render();
 };
